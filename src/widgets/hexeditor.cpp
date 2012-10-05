71a5d380103dcac6c077db92157708efbc7c2c24
Gris87 <Gris87@yandex.ru>
2012-10-05 22:46:15 +0400

    HexEditor

--- src/widgets/hexeditor.cpp
+++ src/widgets/hexeditor.cpp
@@ -1,6 +1,156 @@
 #include "hexeditor.h"
 
 HexEditor::HexEditor(QWidget *parent) :
-    QWidget(parent)
+    QScrollArea(parent)
 {
+}
+
+int HexEditor::indexOf(const QByteArray &aArray, int aFrom) const
+{
+    return 0;
+}
+
+int HexEditor::lastIndexOf(const QByteArray &aArray, int aFrom) const
+{
+    return 0;
+}
+
+void HexEditor::insert(int aIndex, const QByteArray &aArray)
+{
+
+}
+
+void HexEditor::insert(int aIndex, char aChar)
+{
+
+}
+
+void HexEditor::remove(int aPos, int aLength)
+{
+
+}
+
+void HexEditor::replace(int aPos, int aLength, const QByteArray &aArray)
+{
+
+}
+
+QString HexEditor::toString()
+{
+    return "";
+}
+
+void HexEditor::undo()
+{
+
+}
+
+void HexEditor::redo()
+{
+
+}
+
+void HexEditor::setAddressVisible(const bool &aAddressVisible)
+{
+
+}
+
+void HexEditor::setTextVisible(const bool &aTextVisible)
+{
+
+}
+
+void HexEditor::setHighlightingEnabled(const bool &aEnable)
+{
+
+}
+
+QByteArray HexEditor::data() const
+{
+    return QByteArray();
+}
+
+void HexEditor::setData(QByteArray const &aData)
+{
+
+}
+
+HexEditor::Mode HexEditor::mode() const
+{
+    return INSERT;
+}
+
+void HexEditor::setMode(const Mode &aMode)
+{
+
+}
+
+bool HexEditor::isReadOnly() const
+{
+    return false;
+}
+
+void HexEditor::setReadOnly(const bool &aReadOnly)
+{
+
+}
+
+QFont HexEditor::font() const
+{
+    return QFont();
+}
+
+void HexEditor::setFont(const QFont &aFont)
+{
+
+}
+
+int HexEditor::addressOffset() const
+{
+    return 0;
+}
+
+void HexEditor::setAddressOffset(int aOffset)
+{
+
+}
+
+int HexEditor::cursorPosition() const
+{
+    return 0;
+}
+
+void HexEditor::setCursorPosition(int cusorPos)
+{
+
+}
+
+QColor HexEditor::addressBackgroundColor() const
+{
+    return QColor();
+}
+
+void HexEditor::setAddressBackgroundColor(QColor const &color)
+{
+
+}
+
+QColor HexEditor::highlightingColor() const
+{
+    return QColor();
+}
+
+void HexEditor::setHighlightingColor(QColor const &color)
+{
+
+}
+
+QColor HexEditor::selectionColor() const
+{
+    return QColor();
+}
+
+void HexEditor::setSelectionColor(QColor const &color)
+{
+
 }
--- src/widgets/hexeditor.h
+++ src/widgets/hexeditor.h
@@ -1,13 +1,90 @@
 #ifndef HEXEDITOR_H
 #define HEXEDITOR_H
 
-#include <QWidget>
+#include <QScrollArea>
 
-class HexEditor : public QWidget
+class HexEditor : public QScrollArea
 {
     Q_OBJECT
+
 public:
-    explicit HexEditor(QWidget *parent = 0);
+    Q_PROPERTY(QByteArray   Data                     READ data                     WRITE setData)
+    Q_PROPERTY(Mode         Mode                     READ mode                     WRITE setMode)
+    Q_PROPERTY(bool         ReadOnly                 READ isReadOnly               WRITE setReadOnly)
+    Q_PROPERTY(QFont        Font                     READ font                     WRITE setFont)
+    Q_PROPERTY(int          AddressOffset            READ addressOffset            WRITE setAddressOffset)
+    Q_PROPERTY(int          CursorPosition           READ cursorPosition           WRITE setCursorPosition)
+    Q_PROPERTY(QColor       AddressBackgroundColor   READ addressBackgroundColor   WRITE setAddressBackgroundColor)
+    Q_PROPERTY(QColor       HighlightingColor        READ highlightingColor        WRITE setHighlightingColor)
+    Q_PROPERTY(QColor       SelectionColor           READ selectionColor           WRITE setSelectionColor)
+
+
+
+    Q_ENUMS(Mode)
+
+    enum Mode
+    {
+        INSERT,
+        OVERWRITE
+    };
+
+
+
+    HexEditor(QWidget *parent = 0);
+
+
+
+    int indexOf(const QByteArray &aArray, int aFrom=0) const;
+    int lastIndexOf(const QByteArray &aArray, int aFrom=0) const;
+    void insert(int aIndex, const QByteArray &aArray);
+    void insert(int aIndex, char aChar);
+    void remove(int aPos, int aLength=1);
+    void replace(int aPos, int aLength, const QByteArray &aArray);
+    QString toString();
+
+
+
+    QByteArray data() const;
+    void setData(QByteArray const &aData);
+
+    Mode mode() const;
+    void setMode(const Mode &aMode);
+
+    bool isReadOnly() const;
+    void setReadOnly(const bool &aReadOnly);
+
+    QFont font() const;
+    void setFont(const QFont &aFont);
+
+    int addressOffset() const;
+    void setAddressOffset(int aOffset);
+
+    int cursorPosition() const;
+    void setCursorPosition(int cusorPos);
+
+    QColor addressBackgroundColor() const;
+    void setAddressBackgroundColor(QColor const &color);
+
+    QColor highlightingColor() const;
+    void setHighlightingColor(QColor const &color);
+
+    QColor selectionColor() const;
+    void setSelectionColor(QColor const &color);
+
+protected:
+
+public slots:
+    void undo();
+    void redo();
+    void setAddressVisible(const bool &aAddressVisible);
+    void setTextVisible(const bool &aTextVisible);
+    void setHighlightingEnabled(const bool &aEnable);
+
+signals:
+    void positionChanged(int aPosition);
+    void sizeChanged(int aSize);
+    void dataChanged();
+    void modeChanged(bool aMode);
 };
 
 #endif // HEXEDITOR_H
